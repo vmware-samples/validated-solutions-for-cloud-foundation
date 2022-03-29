878f7954ab1497ac1cad453eac5e91d13143a1d9
Ryan Johnson <johnsonryan@vmware.com>
2022-03-29 11:51:56 -0400

    Update variables.tf

--- alb/terraform-examples/certificate-renewal/variables.tf
+++ alb/terraform-examples/certificate-renewal/variables.tf
@@ -1,36 +1,69 @@
 variable "avi_username" {
-  type    = string
-  description = <Avi Username>
+  type        = string
+  description = "Avi Username"
 }
 
 variable "avi_controller" {
-  type    = string
-  description = <Avi Controller IP/FQDN>
+  type        = string
+  description = "Avi Controller IP/FQDN"
 }
 variable "avi_password" {
-  type    = string
-  description = <Avi Password>
+  type        = string
+  description = "Avi Password"
 }
 
 variable "avi_tenant" {
-  type    = string
-  description = <Avi Tenant>
+  type        = string
+  description = "Avi Tenant"
 }
 
 variable "avi_version" {
-  type    = string
-  description = <Avi COntroller Version>
+  type        = string
+  description = "Avi COntroller Version"
 }
 
 variable "cert_name" {
-  type    = string
-  description = <Avi Certificate Name>
+  type        = string
+  description = "Avi Certificate Name"
 }
 
 variable "cert_file" {
-  type    = string
-  description = <Certificate File name that is stored locally>
+  type        = string
+  description = "Certificate File name that is stored locally"
 }
 
 
 
+variable "avi_username" {
+  type        = string
+  description = "Avi Username"
+}
+
+variable "avi_controller" {
+  type        = string
+  description = "Avi Controller IP/FQDN"
+}
+variable "avi_password" {
+  type        = string
+  description = "Avi Password"
+}
+
+variable "avi_tenant" {
+  type        = string
+  description = "Avi Tenant"
+}
+
+variable "avi_version" {
+  type        = string
+  description = "Avi COntroller Version"
+}
+
+variable "cert_name" {
+  type        = string
+  description = "Avi Certificate Name"
+}
+
+variable "cert_file" {
+  type        = string
+  description = "Certificate File name that is stored locally"
+}
