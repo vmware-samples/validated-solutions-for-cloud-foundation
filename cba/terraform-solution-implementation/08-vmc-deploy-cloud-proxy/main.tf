3afffa509c7fff44748f29517ad543c35a4285eb
Gary Blake <31245616+GaryJBlake@users.noreply.github.com>
2022-08-25 18:28:08 +0100

    feat: Add enhancements to Cloud Proxy deployments (#8)
    Adds enhancements to Cloud Proxy deployments:
    - Adds automatic collection of the One Time Key (OTK) from VMC
    - Adds the use of the `terracurl` provider for REST API Calls
    
    Signed-off-by: Gary Blake <gblake@vmware.com>
    Co-authored-by: Ryan Johnson <johnsonryan@vmware.com>

--- cba/terraform-solution-implementation/08-vmc-deploy-cloud-proxy/main.tf
+++ cba/terraform-solution-implementation/08-vmc-deploy-cloud-proxy/main.tf
@@ -2,6 +2,10 @@
 # PROVIDERS
 ##################################################################################
 
+provider "terracurl" {
+  # Configuration options
+}
+
 provider "vsphere" {
   vsphere_server       = var.vsphere_server
   user                 = var.vsphere_username
@@ -9,9 +13,23 @@ provider "vsphere" {
   allow_unverified_ssl = var.vsphere_insecure
 }
 
-##################################################################################
+# ##################################################################################
 # DATA
-##################################################################################
+# ##################################################################################
+
+data "terracurl_request" "get_ova_url" {
+  depends_on = [terracurl_request.get_access_token]
+  name       = "ova_url"
+  url        = "${var.vra_uri}/api/artifact-provider?artifact=data-collector"
+  method     = "GET"
+  headers = {
+    Accept        = "application/json"
+    Content-Type  = "application/json"
+    Authorization = "Bearer ${jsondecode(terracurl_request.get_access_token.response).access_token}"
+  }
+}
+
+# VMware vSphere
 
 data "vsphere_datacenter" "datacenter" {
   name = var.vsphere_datacenter
@@ -38,22 +56,65 @@ data "vsphere_host" "host" {
 }
 
 data "vsphere_ovf_vm_template" "ovf" {
+  depends_on       = [data.terracurl_request.get_ova_url]
   name             = var.cloud_proxy_name
+  remote_ovf_url   = var.cloud_proxy_ovf_remote
   resource_pool_id = data.vsphere_resource_pool.pool.id
   datastore_id     = data.vsphere_datastore.datastore.id
   host_system_id   = data.vsphere_host.host.id
-  remote_ovf_url   = var.cloud_proxy_ovf_remote
-  local_ovf_path   = var.cloud_proxy_ovf_local
   ovf_network_map = {
     "Network 1" : data.vsphere_network.network.id
   }
 }
 
-##################################################################################
+# ##################################################################################
 # RESOURCES
-##################################################################################
+# ##################################################################################
+
+# Obtain Access Token from VMware Cloud Service
+resource "terracurl_request" "get_access_token" {
+  name           = "access_token"
+  url            = "${var.csp_uri}/csp/gateway/am/api/auth/api-tokens/authorize?refresh_token=${var.csp_api_token}"
+  method         = "POST"
+  response_codes = [200, 400, 404, 409, 429, 500]
+  headers = {
+    Content-Type = "application/x-www-form-urlencoded"
+  }
+
+  destroy_response_codes = []
+  destroy_url            = ""
+  destroy_method         = ""
+}
+
+# Obtain the One Time Key (OTK) from VMware Cloud Service
+resource "terracurl_request" "get_otk" {
+  depends_on     = [terracurl_request.get_access_token]
+  name           = "ova_url"
+  url            = "${var.vra_uri}/api/otk-v3"
+  method         = "POST"
+  response_codes = [200, 400, 404, 409, 429, 500]
+  headers = {
+    Accept        = "application/json"
+    Content-Type  = "application/json"
+    Authorization = "Bearer ${jsondecode(terracurl_request.get_access_token.response).access_token}"
+  }
+  request_body = <<EOF
+{
+  "url": "${var.vra_uri}",
+  "service":"cloud_assembly"
+}
+EOF
+
+  destroy_response_codes = []
+  destroy_url            = ""
+  destroy_method         = ""
+}
 
 resource "vsphere_virtual_machine" "cloud_proxy" {
+  depends_on = [
+    terracurl_request.get_otk,
+    data.vsphere_ovf_vm_template.ovf
+  ]
   name                 = var.cloud_proxy_name
   folder               = var.vsphere_folder
   resource_pool_id     = data.vsphere_resource_pool.pool.id
@@ -73,14 +134,14 @@ resource "vsphere_virtual_machine" "cloud_proxy" {
   wait_for_guest_net_timeout = 0
   wait_for_guest_ip_timeout  = 0
   ovf_deploy {
-    remote_ovf_url    = var.cloud_proxy_ovf_remote
-    local_ovf_path    = var.cloud_proxy_ovf_local
-    disk_provisioning = var.cloud_proxy_disk_provisioning
-    ovf_network_map   = data.vsphere_ovf_vm_template.ovf.ovf_network_map
+    allow_unverified_ssl_cert = true
+    remote_ovf_url            = data.vsphere_ovf_vm_template.ovf.remote_ovf_url
+    disk_provisioning         = var.cloud_proxy_disk_provisioning
+    ovf_network_map           = data.vsphere_ovf_vm_template.ovf.ovf_network_map
   }
   vapp {
     properties = {
-      "ONE_TIME_KEY"                 = var.cloud_proxy_otk
+      "ONE_TIME_KEY"                 = jsondecode(terracurl_request.get_otk.response).key
       "itfm_root_password"           = var.cloud_proxy_root_password
       "rdc_name"                     = var.cloud_proxy_display_name
       "network_proxy_hostname_or_ip" = var.cloud_proxy_network_proxy_hostname_ip
@@ -98,7 +159,6 @@ resource "vsphere_virtual_machine" "cloud_proxy" {
 }
 
 resource "null_resource" "cloud_proxy_init" {
-
   provisioner "remote-exec" {
     inline = [
       "chage --mindays ${var.cloud_proxy_root_password_mindays} --maxdays ${var.cloud_proxy_root_password_maxdays} --warndays ${var.cloud_proxy_root_password_warndays} root",
