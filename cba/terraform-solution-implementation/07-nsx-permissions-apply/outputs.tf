806c5ab68a707fc2ee7ab195778dbc96b5508a3b
Gary Blake <gblake@vmware.com>
2022-08-30 17:43:15 +0100

    Enhancement to NSX Permissions Apply
    - Implemented using terracurl provider
    
    Signed-off-by: Gary Blake <gblake@vmware.com>

--- cba/terraform-solution-implementation/07-nsx-permissions-apply/outputs.tf
+++ cba/terraform-solution-implementation/07-nsx-permissions-apply/outputs.tf
@@ -1,3 +1,11 @@
 ##################################################################################
 # OUTPUTS
 ##################################################################################
+
+output "output_nsx_response" {
+  value = jsondecode(terracurl_request.nsx_create_user.response)
+}
+
+output "output_nsx_user_id" {
+  value = jsondecode(terracurl_request.nsx_create_user.response).id
+}
--- cba/terraform-solution-implementation/07-nsx-permissions-apply/versions.tf
+++ cba/terraform-solution-implementation/07-nsx-permissions-apply/versions.tf
@@ -4,13 +4,9 @@
 
 terraform {
   required_providers {
-    nsxt = {
-      source  = "vmware/nsxt"
-      version = "3.2.8"
-    }
-    restapi = {
-      source  = "Mastercard/restapi"
-      version = "1.17.0"
+    terracurl = {
+      source  = "devops-rob/terracurl"
+      version = "0.1.0"
     }
   }
   required_version = ">= 1.2.0"
