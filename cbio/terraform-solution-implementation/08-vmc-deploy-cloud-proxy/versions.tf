0b0fc7e3aa1a9f3ff1bf5aec6ec9faba48bd0ff6
Jason Shiplett <shiplettja@vmware.com>
2022-11-15 14:17:43 -0600

    Initial commmit - cloud proxy
    Signed-off-by: Jason Shiplett <shiplettja@vmware.com>

---
+++ cbio/terraform-solution-implementation/08-vmc-deploy-cloud-proxy/versions.tf
@@ -0,0 +1,21 @@
+##################################################################################
+# VERSIONS
+##################################################################################
+
+terraform {
+  required_providers {
+    vsphere = {
+      source  = "hashicorp/vsphere"
+      version = ">= 2.2.0"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = ">= 3.1.1"
+    }
+    terracurl = {
+      source  = "devops-rob/terracurl"
+      version = "0.1.1"
+    }
+  }
+  required_version = ">= 1.2.0"
+}
