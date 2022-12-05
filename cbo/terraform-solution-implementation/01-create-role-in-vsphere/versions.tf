fbbc0fc0cecd862dcd970ff49e083aa95972e4bd
Jason Shiplett <shiplettja@vmware.com>
2022-12-05 10:28:52 -0600

    Updated vSphere provider version to 2.2.0
    Signed-off-by: Jason Shiplett <shiplettja@vmware.com>

--- cbo/terraform-solution-implementation/01-create-role-in-vsphere/versions.tf
+++ cbo/terraform-solution-implementation/01-create-role-in-vsphere/versions.tf
@@ -6,7 +6,7 @@ terraform {
   required_providers {
     vsphere = {
       source  = "hashicorp/vsphere"
-      version = ">= 2.0.2"
+      version = ">= 2.2.0"
     }
   }
   required_version = ">= 1.0.0"
