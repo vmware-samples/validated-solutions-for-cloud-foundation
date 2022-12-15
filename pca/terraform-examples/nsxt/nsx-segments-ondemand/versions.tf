5913851f2fda0dfc679391ab1883adb9558a0c8f
Ryan Johnson <ryan@tenthirtyam.org>
2022-12-15 13:17:50 -0500

    chore(pca): bump terraform and provider versions
    - Updates Terraform to `>= 1.2.0`
    - Updates Terraform provider for VMware vSphere to `>= 2.2.0`
    - Updates Terraform provider for VMware NSX to `>= 3.2.9`
    - Updates Terraform provider for VMware vRealize Automation to `0.4.1`
    - Removes extra `.gitignore` files.
    
    Signed-off-by: Ryan Johnson <ryan@tenthirtyam.org>

--- pca/terraform-examples/nsxt/nsx-segments-ondemand/versions.tf
+++ pca/terraform-examples/nsxt/nsx-segments-ondemand/versions.tf
@@ -6,8 +6,8 @@ terraform {
   required_providers {
     nsxt = {
       source  = "vmware/nsxt"
-      version = "3.2.1"
+      version = ">= 3.2.9"
     }
   }
-  required_version = ">= 1.0.0"
+  required_version = ">= 1.2.0"
 }
