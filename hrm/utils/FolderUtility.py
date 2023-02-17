8aa0ff9a19ba3216961cf17444228c1be28430cd
bhumitra nagar <bnagar@vmware.com>
2023-02-17 05:15:07 +0000

    Adding Python Module for HRM
    Signed-off-by: bhumitra nagar <bnagar@vmware.com>

---
+++ hrm/utils/FolderUtility.py
@@ -0,0 +1,152 @@
+# Copyright 2022-2023 VMware, Inc.
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+# WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+# COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+# OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+# ===================================================================================================================
+# Created by:  Bhumitra Nagar - Senior Member of Technical Staff
+# Authors: Bhumitra Nagar
+# Date:   2023-02-01
+# Version: 1.0.0.1001
+# ===================================================================================================================
+#
+# Description:
+# Helper script to perform folder operations like creating log directory, and deleting logs older than x days.
+# Logs location and retention period Values are specified in env.json.
+
+import os
+import fnmatch
+import shutil
+import time
+from datetime import datetime
+import json
+
+with open(os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)), '../env.json'))) as f:
+    data = json.load(f)
+
+DEFAULT_LOGS_DIR_PATH_LINUX = data["DEFAULT_LOGS_DIR_PATH_LINUX"]
+DEFAULT_LOGS_DIR_PATH_WINDOWS = data["DEFAULT_LOGS_DIR_PATH_WINDOWS"]  # os.environ["HOMEPATH"] will be prepended to it
+
+
+class FolderUtility(object):
+
+    @staticmethod
+    def get_default_log_path():
+        if os.name == 'posix':
+            logs_dir = DEFAULT_LOGS_DIR_PATH_LINUX
+        else:
+            logs_dir = os.path.join(os.path.join(os.environ["HOMEPATH"], DEFAULT_LOGS_DIR_PATH_WINDOWS))
+        return logs_dir
+
+    @staticmethod
+    def get_log_directory():
+        logs_dir = FolderUtility.get_default_log_path()
+        if not os.path.exists(logs_dir):
+            os.makedirs(logs_dir)
+        return logs_dir
+
+    @staticmethod
+    def make_directory(path):
+        """
+        Make directory at given path
+        """
+        if not os.path.isdir(path):
+            os.makedirs(path)
+
+    @staticmethod
+    def make_director_with_timestamp(path):
+        dir_name = path + datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+        os.makedirs(dir_name)
+        return dir_name
+
+    @staticmethod
+    def make_unique_directory(path):
+        base_name = path
+        count = 0
+        temp_dir_name = base_name + str(count)
+        while os.path.isdir(temp_dir_name):
+            count += 1
+            temp_dir_name = base_name + str(count)
+        os.makedirs(temp_dir_name)
+        return temp_dir_name
+
+    @staticmethod
+    def get_unique_file_path(path):
+        base_name, ext = os.path.splitext(path)
+        count = 0
+        temp_file_name = base_name + str(count) + ext
+        while os.path.isfile(temp_file_name):
+            count += 1
+            temp_file_name = base_name + str(count) + ext
+        return temp_file_name
+
+    @staticmethod
+    def search_file_in_path(file_name, path):
+        """
+        By default it searches for .py file recursively inside given path
+        file_name and path can be changed.
+        Raise exception of no matches or multiple matches are found
+        :return: complete path of the given file.
+        """
+        if '.' not in file_name:
+            file_name = file_name + '.py'
+        matches = []
+        for root, dir_names, file_names in os.walk(path):
+            for filename in fnmatch.filter(file_names, file_name):
+                matches.append(os.path.join(root, filename))
+
+        if len(matches) == 0:
+            raise Exception('File not found')
+        if len(matches) > 1:
+            raise Exception('Multiple files found with same name')
+        else:
+            return matches[0]
+
+    @staticmethod
+    def delete_logs_older_than_days(days=30, logger=None):
+        deleted_folders_count = 0
+        deleted_files_count = 0
+
+        path = FolderUtility.get_default_log_path()
+
+        seconds = time.time() - (days * 24 * 60 * 60)
+
+        if os.path.exists(path):
+            for root_folder, folders, files in os.walk(path):
+                for folder in folders:
+                    if 'run_' in folder:
+                        folder_path = os.path.join(root_folder, folder)
+                        if seconds >= os.stat(folder_path).st_ctime:
+                            if logger:
+                                logger.info(f'Deleting {folder_path}')
+                            FolderUtility.remove_folder(folder_path)
+                            deleted_folders_count += 1
+                # checking the current directory files
+                for file in files:
+                    file_path = os.path.join(root_folder, file)
+                    if seconds >= os.stat(file_path).st_ctime:
+                        if logger:
+                            logger.info(f'Deleting {file_path}')
+                        FolderUtility.remove_file(file_path)
+                        deleted_files_count += 1
+        else:
+            if logger:
+                logger.info(f'"{path}" is not found')
+
+        if logger:
+            logger.info(f"Total folders deleted: {deleted_folders_count}")
+
+    @staticmethod
+    def remove_folder(path):
+        if not shutil.rmtree(path):
+            print(f"{path} is removed successfully")
+        else:
+            print(f"Unable to delete the {path}")
+
+    @staticmethod
+    def remove_file(path):
+        if not os.remove(path):
+            print(f"{path} is removed successfully")
+        else:
+            print(f"Unable to delete the {path}")
---
+++ hrm/utils/PSUtility.py
@@ -0,0 +1,73 @@
+# Copyright 2022-2023 VMware, Inc.
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+# WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+# COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+# OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+# ===================================================================================================================
+# Created by:  Bhumitra Nagar - Senior Member of Technical Staff
+# Authors: Bhumitra Nagar
+# Date:   2023-02-01
+# Version: 1.0.0.1001
+# ===================================================================================================================
+#
+# Description:
+# Helper script to run health checks and get data from the SOS Utility running on SDDC Manager.
+
+
+import os
+import subprocess
+import signal
+
+
+class PSUtility:
+
+    def __init__(self, logger=None):
+        self.p = None
+        self.logger = logger
+        if os.name == 'posix':
+            pwsh = '/usr/bin/pwsh'
+        else:
+            pwsh = 'powershell.exe'
+        self.cmd_pre = [pwsh, '-command', 'Import-Module', 'PowerValidatedSolutions', ';']
+
+    def log_msg(self, msg):
+        msg_arr = msg.split()
+        for word in msg.split():
+            if word.startswith('-') and 'pass' in word.lower():
+                word_index = msg_arr.index(word)
+                msg_arr[word_index + 1] = '********'
+
+        msg = ' '.join(msg_arr)
+        if self.logger:
+            self.logger.info(msg)
+        else:
+            print(msg)
+
+    def execute_ps_cmd(self, cmd):
+        try:
+            self.log_msg(cmd)
+            full_cmd = self.cmd_pre + cmd.split(' ')
+            self.p = subprocess.Popen(full_cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            stdout, stderr = self.p.communicate()
+            output = stdout.decode('utf-8')
+            err = stderr.decode('utf-8')
+            if err != '':
+                self.log_msg(f'Error encountered while running command - {err}')
+            self.log_msg(f'output received = {output}')
+        except Exception as e:
+            self.log_msg(e)
+        finally:
+            if not self.p:
+                self.log_msg(f'Kill subprocess with id {self.p.pid}')
+                try:
+                    os.killpg(os.getpgid(self.p.pid), signal.SIGTERM)
+                except Exception as e:
+                    self.log_msg(f'error occurred while killing process {e}')
+
+
+if __name__ == "__main__":
+    psu = PSUtility()
+    psu.execute_ps_cmd(
+        'Publish-BackupStatus -server sfo-vcf01.sfo.rainpole.io -user <user> -pass <pass> '
+        '-allDomains -outputJson .')
